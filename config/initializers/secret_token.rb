0c8c507ea6916e08685789b0d581ec7e4201057d
Shogo Kawaguchi <platycod0n.sj2826@gmail.com>
2013-03-26 00:10:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinReportsExample::Application.config.secret_token = 'd1f4e3efe07ad3bd27403a2731add1f8c06e3c7e9976e360a0f5bbeb1d45a10a64291227148ebc97a820371097b0b9641352a8416d81f81d1dd6c58c82be7cc2'
